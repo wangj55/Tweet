e1fda3c44ebcd8900d8f92e2615e3094874859ce
Ji Wang <wj42421999@gmail.com>
2022-03-13 14:42:48 -0400

    User stays logged in across restarts

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -9,33 +9,36 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+
     @IBAction func onLoginButton(_ sender: Any) {
         let loginUrl = "https://api.twitter.com/oauth/request_token"
-        
-        
+
         TwitterAPICaller.client?.login(url: loginUrl, success: {
+            UserDefaults.standard.set(true, forKey: "userLoggedIn") // remember the user is logged in
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { _ in
             print("Could not login")
         })
     }
-    
 
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
+     */
 }
