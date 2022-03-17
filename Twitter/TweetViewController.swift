32cf12607131acd0a40fbdd6d1a453bb2f6c7b2e
Ji Wang <wj42421999@gmail.com>
2022-03-17 19:35:21 -0400

    Implemented save and load tweet draft

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -16,19 +16,25 @@ class TweetViewController: UIViewController, UITextViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadSavedTweet()
         tweetText.becomeFirstResponder()
         tweetText.delegate = self
         wordCountLabel.text = "\(tweetText.text.count)/\(characterLimit)"
     }
 
     @IBAction func cancel(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        saveTweet()
     }
 
     /// Post tweet.
     @IBAction func tweet(_ sender: Any) {
         if !tweetText.text.isEmpty {
             TwitterAPICaller.client?.postTweet(tweetString: tweetText.text, success: {
+                // flush saved tweet text
+                if UserDefaults.standard.object(forKey: "tweetText") != nil {
+                    UserDefaults.standard.removeObject(forKey: "tweetText")
+                }
+
                 self.dismiss(animated: true, completion: nil)
             }, failure: { (error: Error) in
                 print("Error when posting tweet \(error)")
@@ -40,10 +46,38 @@ class TweetViewController: UIViewController, UITextViewDelegate {
         }
     }
 
+    /// Manages the word limit of tweet.
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         let newText = NSString(string: textView.text).replacingCharacters(in: range, with: text)
         wordCountLabel.text = "\(newText.count)/\(characterLimit)"
         return newText.count < characterLimit
+    }
+
+    /// Pop up an alert and save the current tweet that is not finished if it is not empty.
+    func saveTweet() {
+        if !tweetText.text.isEmpty {
+            let saveAlert = UIAlertController(title: "Save Tweet", message: "Do you want to save this tweet as draft?", preferredStyle: UIAlertController.Style.alert)
+
+            saveAlert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { _ in
+                UserDefaults.standard.set(self.tweetText.text, forKey: "tweetText")
+                self.dismiss(animated: true, completion: nil)
+            }))
+
+            saveAlert.addAction(UIAlertAction(title: "No", style: .cancel, handler: { _ in
+                self.dismiss(animated: true, completion: nil)
+            }))
+
+            view.endEditing(true)
+            present(saveAlert, animated: true, completion: nil)
+        }
+    }
+
+    /// Load tweet content if saved last time.
+    func loadSavedTweet() {
+        let savedText = UserDefaults.standard.string(forKey: "tweetText") ?? ""
+        if !savedText.isEmpty {
+            tweetText.text = savedText
+        }
     }
 
     /*
